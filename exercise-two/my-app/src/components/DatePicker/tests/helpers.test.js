96f72eb41e2ae66563133c5facd8dc710f8c8c1b
Utkarsh Saxena <utkarsh.saxena@shopify.com>
2018-02-08 13:52:29 -0500

    initial commit

--- exercise-two/my-app/src/components/DatePicker/tests/helpers.test.js
+++ exercise-two/my-app/src/components/DatePicker/tests/helpers.test.js
@@ -1,9 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { getYearForRange } from "../helpers";
+import DateMock from "./mocks/DateMock";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("getYearForRange", () => {
+  const originalDate = window.Date;
+
+  afterEach(() => {
+    window.Date = originalDate;
+  });
+
+  it("gets the year when start date is provided", () => {
+    const year = 1969;
+    const result = getYearForRange({
+      start: new Date(`July 20, ${year} 00:20:18`)
+    });
+
+    expect(result).toBe(year);
+  });
+
+  it("gets the year when end date is provided", () => {
+    const year = 1969;
+
+    const result = getYearForRange({
+      end: new Date(`July 20, ${year} 00:20:18`)
+    });
+
+    expect(result).toBe(year);
+  });
+
+  it("gets the start year when both start and end date is provided", () => {
+    const start = 1949;
+    const end = 1969;
+
+    const result = getYearForRange({
+      start: new Date(`July 20, ${start} 00:20:18`),
+      end: new Date(`July 20, ${end} 00:20:18`)
+    });
+
+    expect(result).toBe(start);
+  });
+
+  it("gets the current year when start and end date are not provided", () => {
+    const currentYear = 2016;
+    window.Date = DateMock(new Date(`July 20, ${currentYear} 00:20:18`));
+    const result = getYearForRange();
+    expect(result).toBe(currentYear);
+  });
 });
