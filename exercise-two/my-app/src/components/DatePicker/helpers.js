92d849effb85d76aaf271de74d0d5e30de64ad08
Utkarsh Saxena <utkarsh.saxena@shopify.com>
2018-02-08 13:52:29 -0500

    initial commit

---
+++ exercise-two/my-app/src/components/DatePicker/helpers.js
@@ -0,0 +1,11 @@
+export function getYearForRange(range) {
+  if (range && range.start) {
+    return range.start.getFullYear();
+  }
+
+  if (range && range.end) {
+    return range.end.getFullYear();
+  }
+
+  return new Date().getFullYear();
+}
