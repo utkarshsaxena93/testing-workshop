3ab1a44f8cf2c93415d74bade4937dd8dc700f25
Utkarsh Saxena <utkarsh.saxena@shopify.com>
2018-02-15 17:30:50 -0500

    prop tests

--- ReactTesting/myApp/src/DatePicker/tests/DatePicker.test.js
+++ ReactTesting/myApp/src/DatePicker/tests/DatePicker.test.js
@@ -1,13 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { mount } from "enzyme";
 
-describe('<Month />', () => {
-  it('sets the selected prop on the Day component to true', () => {
+import Month from "../Month";
+import Day from "../Day";
 
-  });
+describe("<Month />", () => {
+  const mockProps = {
+    date: 22,
+    month: 10,
+    year: 2018,
+    onDayClick: () => {}
+  };
 
-  it('sets the selected prop on the Day component to false', () => {
-
+  describe("<Day />", () => {
+    it("sets the selected prop on the Day component to true for the given date", () => {
+      const date = 15;
+      const wrapper = mount(<Month {...mockProps} date={date} />);
+      const selectedDayComponent = wrapper.find(Day).filterWhere(component => {
+        const fullDate = component.prop("fullDate");
+        if (fullDate == null) {
+          return false;
+        }
+        return fullDate.getDate() === date;
+      });
+  
+      expect(selectedDayComponent.prop("selected")).toBe(true);
+    });
+  
+    it('assigns the onClick prop to the Day component', () => {
+      const onDayClickSpy = () => {};
+      const wrapper = mount(<Month {...mockProps} onDayClick={onDayClickSpy} />);
+      const nonEmptyStateDayComponents = wrapper.find(Day).filterWhere(component => {
+        const fullDate = component.prop("fullDate");
+        if (fullDate == null) {
+          return false;
+        }
+        return fullDate.getDate();
+      });
+  
+      const firstDayComponent = nonEmptyStateDayComponents.first();
+  
+      expect(firstDayComponent.prop('onClick')).toBe(onDayClickSpy);
+    });
+  
+    it('calls the onDayClick callback when Day is clicked', () => {
+      const onDayClickSpy = jest.fn();
+      const wrapper = mount(<Month {...mockProps} onDayClick={onDayClickSpy} />);
+      const nonEmptyStateDayComponents = wrapper.find(Day).filterWhere(component => {
+        const fullDate = component.prop("fullDate");
+        if (fullDate == null) {
+          return false;
+        }
+        return fullDate.getDate();
+      });
+  
+      const firstDayComponent = nonEmptyStateDayComponents.first();
+  
+      firstDayComponent.simulate('click');
+      expect(onDayClickSpy).toHaveBeenCalledTimes(1);
+    });
   });
 });
