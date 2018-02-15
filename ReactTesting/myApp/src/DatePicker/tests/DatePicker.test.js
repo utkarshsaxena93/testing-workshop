e00a62d2f0fdb3afe95ad1849940decf9a0f44d7
Utkarsh Saxena <utkarsh.saxena@shopify.com>
2018-02-15 13:04:14 -0500

    set up testing

---
+++ ReactTesting/myApp/src/DatePicker/tests/DatePicker.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+describe('<Month />', () => {
+  it('sets the selected prop on the Day component to true', () => {
+
+  });
+
+  it('sets the selected prop on the Day component to false', () => {
+
+  });
+});
